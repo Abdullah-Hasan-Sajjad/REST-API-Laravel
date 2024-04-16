8f8a1821d4566099f13b0f85e3ac543d728f6414
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-04-16 13:36:39 +0600

    car model, migration and seeder created

---
+++ REST-API-Laravel/database/factories/CarFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Type\Integer;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Car>
+ */
+class CarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'car_name' => fake()->name(),
+            'company_name' => fake()->company(),
+            'model' => fake()->companySuffix(),
+            'price' => fake()->randomDigit(),
+        ];
+    }
+}
